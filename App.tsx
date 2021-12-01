48e97ad581f323fb5fb21fe8b7411dac6e9789d8
Nicole Janeway <bnicolebills@gmail.com>
2021-12-01 14:07:32 -0600

    radio buttons

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import Pomodoro from './Pomodoro';
+import Pomodoro from "./Pomodoro";
+import Details from "./Details";
 
 const Stack = createNativeStackNavigator();
 
@@ -11,13 +12,8 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Details" component={Details} />
-        <Stack.Screen name="clock" component={Pomodoro}/>
+        <Stack.Screen name="clock" component={Pomodoro} />
       </Stack.Navigator>
-  </NavigationContainer>
+    </NavigationContainer>
   );
 }
-
-
-
-
-
--- Details.tsx
+++ Details.tsx
@@ -1,37 +1,52 @@
-import React, {useState, useRef, useEffect} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useRef, useEffect } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import { RadioButton } from "react-native-paper";
 
-import SetTimer from './SetTimer';
-import TimerDisplay from './TimerDisplay'
+export default function Details({ navigation }) {
+  const [sessionLength, setSessionLength] = useState(25);
+  const [task, setTask] = useState("");
 
-export default function Details() {
-    const [sessionLength, setSessionLength] = useState(25);
-    const [breakLength, setBreakLength] = useState(5);
-    const [task, setTask] = useState("");
-
-    return (<>
-        <View style={styles.container}>
-            <Text>
-                Set task
-            </Text>
-                <TextInput
-                    onChangeText={setTask}
-                    value={task}
-                    style = {{ width: 20, textAlign: 'center' }}
-                />
-            <Text>
-                Set pomodoro
-            </Text>    
-        </View>
-     </>
-    );
+  return (
+    <>
+      <View style={styles.container}>
+        <Text>Set task</Text>
+        <TextInput
+          onChangeText={setTask}
+          value={task}
+          style={{ width: 200, textAlign: "center" }}
+        />
+        <Text>Set pomodoro</Text>
+        <RadioButton
+          value="25"
+          status={sessionLength === 25 ? "checked" : "unchecked"}
+          onPress={() => setSessionLength(25)}
+        >
+          <Text>25 minutes</Text>
+        </RadioButton>
+        <RadioButton
+          value="32"
+          status={sessionLength === 32 ? "checked" : "unchecked"}
+          onPress={() => setSessionLength(32)}
+        >
+          32 minutes
+        </RadioButton>
+        <RadioButton
+          value="50"
+          status={sessionLength === 50 ? "checked" : "unchecked"}
+          onPress={() => setSessionLength(50)}
+        >
+          50 minutes
+        </RadioButton>
+        <Button title="Start" onPress={navigation.navigate("clock")}>
+      </View>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flexDirection: "row",
-      alignItems: 'center',
-      justifyContent: 'center',
-      padding: 4,
-    },
-  });
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 4,
+  },
+});
